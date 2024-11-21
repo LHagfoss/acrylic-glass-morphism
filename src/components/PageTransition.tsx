cbdeabb3ead81b8fc3a6c738cf6cecd7c8cd099b
LHagfoss <afs0028447@agderskolen.no>
2024-11-21 21:50:31 +0100

    blur transition

--- src/components/PageTransition.tsx
+++ src/components/PageTransition.tsx
@@ -11,30 +11,36 @@ export default function PageTransition() {
       }, []);
 
     return (
-        <motion.div className="fixed top-0 left-0 z-30 w-[100vw] h-[100vh]" hidden={hideMe}>
-            <div className={`flex justify-center ${hideMe ? "hidden" : ""}`}>
-                {Array.from({ length: 5 }).map((_, i) => (
-                    <motion.div
-                    key={i}
-                    className="w-full h-[50vh] bg-[#0a0a0a] box"
-                    initial={{ y: 0 }}
-                    animate={{ translateY: '-110%' }}
-                    transition={{ delay: 1 + i * 0.2, duration: 1, ease: easeIn }}
-                    />
-                ))}
-                </div>
+        <motion.div 
+            initial={{ backdropFilter: "blur(100px)" }}
+            animate={{ backdropFilter: "blur(0px)" }}
+            transition={{ delay: 1, duration: 2 }}
+            className="z-30 w-[100vw] h-[100vh] fixed top-0 left-0"></motion.div>
+            
+    //     <motion.div className="fixed top-0 left-0 z-30 w-[100vw] h-[100vh]" hidden={hideMe}>
+    //         <div className={`flex justify-center ${hideMe ? "hidden" : ""}`}>
+    //             {Array.from({ length: 5 }).map((_, i) => (
+    //                 <motion.div
+    //                 key={i}
+    //                 className="w-full h-[50vh] bg-[#0a0a0a] box"
+    //                 initial={{ y: 0 }}
+    //                 animate={{ translateY: '-110%' }}
+    //                 transition={{ delay: 1 + i * 0.1, duration: 0.5, ease: easeIn }}
+    //                 />
+    //             ))}
+    //             </div>
                 
-                <div className={`flex justify-center ${hideMe ? "hidden" : ""}`}>
-                {Array.from({ length: 5 }).map((_, i) => (
-                    <motion.div
-                    key={i}
-                    className="w-full h-[50vh] bg-[#0a0a0a] box"
-                    initial={{ y: 0 }}
-                    animate={{ translateY: '110%' }}
-                    transition={{ delay: 1 + i * 0.2, duration: 1, ease: easeIn }}
-                    />
-                ))}
-            </div>
-      </motion.div>
+    //             <div className={`flex justify-center ${hideMe ? "hidden" : ""}`}>
+    //             {Array.from({ length: 5 }).map((_, i) => (
+    //                 <motion.div
+    //                 key={i}
+    //                 className="w-full h-[50vh] bg-[#0a0a0a] box"
+    //                 initial={{ y: 0 }}
+    //                 animate={{ translateY: '110%' }}
+    //                 transition={{ delay: 1 + i * 0.1, duration: 0.5, ease: easeIn }}
+    //                 />
+    //             ))}
+    //         </div>
+    //   </motion.div>
     )
 }
